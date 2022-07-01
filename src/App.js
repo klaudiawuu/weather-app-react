96311414e48a6f7b8e6b36b6cbb090b49c90ec17
klaudiawuu <klaudia.wawrzynczyk@gmail.com>
2022-07-01 22:32:46 +0200

    URL added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <h1>Weather app</h1>
+      <footer>
+        App created by Klaudia Wawrzynczyk. Please find the code source{" "}
+        <a
+          href="https://github.com/klaudiawuu/weather-app-react"
+          target="_blank"
+          rel="noopener"
+        >
+          here
+        </a>
+      </footer>
     </div>
   );
 }
