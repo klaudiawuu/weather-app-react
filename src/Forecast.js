42544c16127f4ab11a5fd1c9398b0e872fd5a806
klaudiawuu <klaudia.wawrzynczyk@gmail.com>
2022-07-03 17:37:25 +0200

    API added

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,60 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Forecast.css";
 
 export default function Forecast() {
-  return (
-    <div className="Forecast">
-      <form>
+  const [weatherDetails, setWeatherDetails] = useState({ ready: false });
+  function getResponse(response) {
+    console.log(response);
+    setWeatherDetails({
+      ready: true,
+      city: response.data.name,
+      day: "Friday 10:00",
+      description: response.data.weather[0].description,
+      icon: "https://i.ibb.co/mCRPjrR/sunbehindcloud.png",
+      temperature: response.data.main.temp,
+      realFeel: response.data.main.feels_like,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  if (weatherDetails.ready) {
+    return (
+      <div className="Forecast">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Your city"
+                className="searchArea form-control"
+              ></input>
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-outline-secondary"
+              ></input>
+            </div>
+          </div>
+        </form>
+        <h1>{weatherDetails.city}</h1>
+        <ul className="actualTime">
+          <li>{weatherDetails.day}</li>
+          <li className="text-capitalize">{weatherDetails.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Your city"
-              className="searchArea form-control"
-            ></input>
+          <div className="col-6">
+            <img
+              src={weatherDetails.icon}
+              alt="Partly cloudy"
+              className="mainIcon float-left"
+            ></img>
+            <strong className="currentTemp">
+              {Math.round(weatherDetails.temperature)}
+            </strong>
+            <span className="units">°C|°F</span>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-outline-secondary"
-            ></input>
+          <div className="col-6">
+            <ul>
+              <li>Real Feel: {Math.round(weatherDetails.realFeel)}°C</li>
+              <li>Humidity: {weatherDetails.humidity}%</li>
+              <li>Wind: {Math.round(weatherDetails.wind)} km/h</li>
+            </ul>
           </div>
         </div>
-      </form>
-      <h1>Varsovia</h1>
-      <ul className="actualTime">
-        <li>Friday 10:00</li>
-        <li>Partly cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://i.ibb.co/mCRPjrR/sunbehindcloud.png"
-            alt="Partly cloudy"
-            className="mainIcon float-left"
-          ></img>
-          <strong className="currentTemp">25</strong>
-          <span className="units">°C|°F</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Real Feel: 27°C</li>
-            <li>Humidity: 87%</li>
-            <li>Wind: 7 km/h</li>
-          </ul>
-        </div>
+        <footer>
+          App created by Klaudia Wawrzynczyk. Please find the code source{" "}
+          <a
+            href="https://github.com/klaudiawuu/weather-app-react"
+            target="_blank"
+            rel="noopener"
+          >
+            here
+          </a>
+        </footer>
       </div>
-      <footer>
-        App created by Klaudia Wawrzynczyk. Please find the code source{" "}
-        <a
-          href="https://github.com/klaudiawuu/weather-app-react"
-          target="_blank"
-          rel="noopener"
-        >
-          here
-        </a>
-      </footer>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "a367566821d5256a1c920a360eab8e9e";
+    let city = "Lisbon";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(getResponse);
+
+    return "Awaiting response...";
+  }
 }
